28244aa2e0cd55dbb5690a430f00a40a7682c0a0
Juls1111 <99805587+Juls1111@users.noreply.github.com>
2022-05-05 17:05:14 +0300

    Update def_database_connection.py

--- def_database_connection.py
+++ def_database_connection.py
@@ -6,7 +6,7 @@ root = Tk()
 root.title("Statistics")
 root.geometry('440x250')
 root.resizable(width=0, height=0)
-root["bg"]= "#AFFBC7"
+root["bg"] = "#AFFBC7"
 
 
 def getInfoEelement(statistics, table_name, country, year):
@@ -31,9 +31,11 @@ def getInfoEelement(statistics, table_name, country, year):
 
 def getInfoButton():
     result_info = ""
-    result_info = getInfoEelement(default_statistics.get(), default_table_name.get(), default_country.get(), default_year.get())
+    result_info = getInfoEelement(default_statistics.get(), default_statistics.get(), default_country.get(),
+                                  default_year.get())
     result_label = Label(root, text=f"{result_info}", width=10)
-    result_label.grid(column=1, row=6)
+    result_label.place(x=10, y=200)
+
 
 
 def getCountriesInTable(table_name):
@@ -56,27 +58,30 @@ def getCountriesInTable(table_name):
     return ["".join(country).strip("(").strip(")").strip(",").strip("'") for country in cur.fetchall()]
 
 
+statistics_label = Label(root, text="Statistics", font="Broadway 12", width=11, fg="#3C8B57")
+statistics_label.grid(column=0, row=0, ipadx=1, ipady=2, padx=12, pady=10)
 
-statistics_label = Label(root, text="Statistics",font = "Broadway 12", width=11, fg = "#3C8B57")
-statistics_label.grid(column=0, row=0 ,ipadx=1, ipady=2, padx=12, pady=10)
-
-country_label = Label(root, text="Country",font = "Broadway 12", width=10,fg ="#3C8B57")
-country_label.grid(column=1, row=0,ipadx=2, ipady=2, padx=11, pady=10)
+country_label = Label(root, text="Country", font="Broadway 12", width=10, fg="#3C8B57")
+country_label.grid(column=1, row=0, ipadx=2, ipady=2, padx=11, pady=10)
 
 default_statistics = StringVar()
-statistics_options = OptionMenu(root, default_statistics, "life_expectancy")
+
+options = ["life_expectancy", "people_below_poverty_line", "gdp_per_capita"]
+
+statistics_options = OptionMenu(root, default_statistics, *options)
+
 statistics_options.grid(column=0, row=1)
-statistics_options.config(bg= "#EDFFF3")
+statistics_options.config(bg="#EDFFF3")
 statistics_options.config(width=14)
 statistics_options.config(height=1)
 
-year_label = Label(root, text="Year",font = "Broadway 12", width=10,fg ="#3C8B57")
-year_label.grid(column=2, row=0,ipadx=2, ipady=2, padx=11, pady=10)
+year_label = Label(root, text="Year", font="Broadway 12", width=10, fg="#3C8B57")
+year_label.grid(column=2, row=0, ipadx=2, ipady=2, padx=11, pady=10)
 
-year = StringVar()
-entry_year = Entry(root, textvariable = year)
+default_year = StringVar()
+entry_year = Entry(root, textvariable=default_year)
 entry_year.grid(column=2, row=1, ipadx=0, ipady=0, padx=0, pady=0)
-entry_year.config(bg= "#EDFFF3")
+entry_year.config(bg="#EDFFF3")
 entry_year.config(width=20)
 
 countries = getCountriesInTable("life_expectancy")
@@ -84,7 +89,7 @@ countries = getCountriesInTable("life_expectancy")
 default_country = StringVar()
 country_options = OptionMenu(root, default_country, *countries)
 country_options.grid(column=1, row=1)
-country_options.config(bg= "#EDFFF3")
+country_options.config(bg="#EDFFF3")
 country_options.config(width=13)
 country_options.config(height=1)
 
@@ -106,7 +111,8 @@ def getRealNumberStatistics(statistics, country):
     cur = conn.cursor()
     getInfo_script = f"SELECT {statistics} FROM {statistics} WHERE country = '{country}';"
     cur.execute(getInfo_script)
-    return list(map(float,[stats.strip("(").strip(")").strip(",") for stats in list(map(str,cur.fetchall()))]))
+    return list(map(float, [stats.strip("(").strip(")").strip(",") for stats in list(map(str, cur.fetchall()))]))
+
 
 def getYears(statistics, country):
     hostname = 'localhost'
@@ -128,25 +134,27 @@ def getYears(statistics, country):
     return list(map(int, [years.strip("(").strip(")").strip(",") for years in list(map(str, cur.fetchall()))]))
 
 
-def plotData(years,statistics,country,statistics_name):
-    plt.bar(years,statistics)
+def plotData(years, statistics, country, statistics_name):
+    plt.bar(years, statistics)
     plt.xlim(1950, 2025)
     plt.xlabel("Year")
     plt.ylabel("Statistics")
     plt.title(f"{country} {statistics_name}")
     plt.show()
 
+
 def plotDataButton():
-    years = getYears(default_statistics.get(),default_country.get())
-    statistics = getRealNumberStatistics(default_statistics.get(),default_country.get())
-    plotData(years,statistics,default_country.get(),default_statistics.get())
+    years = getYears(default_statistics.get(), default_country.get())
+    statistics = getRealNumberStatistics(default_statistics.get(), default_country.get())
+    plotData(years, statistics, default_country.get(), default_statistics.get())
 
-button_v = Button(root, text="Visualize Data",font = "Broadway 16", bg = "white", fg = "PaleGreen4", activebackground = "snow2")
-button_v.place(x=10, y =100)
 
-button_y = Button(root, text="Statistics Per Year",font = "Broadway 16", bg = "white", fg = "PaleGreen4", activebackground = "snow2")
-button_y.place(x=10, y =150)
+button_v = Button(root, text="Visualize Data", font="Broadway 16", bg="white", fg="PaleGreen4",
+                  activebackground="snow2", command=plotDataButton)
+button_v.place(x=10, y=100)
+
+button_y = Button(root, text="Statistics Per Year", font="Broadway 16", bg="white", fg="PaleGreen4",
+                  activebackground="snow2", command=getInfoButton)
+button_y.place(x=10, y=150)
 
 root.mainloop()
-
- 
